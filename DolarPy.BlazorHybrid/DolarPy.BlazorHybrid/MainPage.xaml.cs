2475b77c9be2763fa7ae9d51fc6d7523fc666a3f
Javier Meza <javier.meza.py@gmail.com>
2025-02-11 23:05:10 -0300

    Agregar proyecto DolarPy.BlazorHybrid

---
+++ DolarPy.BlazorHybrid/DolarPy.BlazorHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DolarPy.BlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
